a5616b1488e5ef74f6db676e7b5381d44832fd9e
RennanAtes <RennanAugustoBR@Gmail.com>
2023-07-10 08:39:22 -0300

    Update ButtonSignInGithub.tsx

--- src/app/components/ButtonSignInGithub.tsx
+++ src/app/components/ButtonSignInGithub.tsx
@@ -1,14 +1,22 @@
-"use client"
+import { signIn } from "next-auth/react";
 
-import { signIn, signOut, useSession } from "next-auth/react"
+export default function ButtonSignInGithub() {
+  async function realizarLoginGithub() {
+    // Obter a URL de retorno após o login com o GitHub
+    const callbackUrl = "http://localhost:3000/callback";
+    
+    // Realizar o login com o provedor GitHub
+    await signIn("github", { callbackUrl });
+  }
 
-
-export default function ButtonSignInGithub(){
-
-    return (
-        <div>
-            <button className="border-2 border-black bg-black text-white rounded-md p-1   "
-             onClick={() => signIn('github')}>Github Login</button>
-        </div>
-    )
+  return (
+    <div>
+      <button
+        className="border-2 border-black bg-black text-white rounded-md p-1"
+        onClick={realizarLoginGithub}
+      >
+        Github Login
+      </button>
+    </div>
+  );
 }
